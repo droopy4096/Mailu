3d184461090316ab7c518fe6efb04dedd8310061
Dmytro Makovey <dmakovey@stanford.edu>
2017-10-04 09:19:10 -0700

    backported rmilter

--- rmilter/start.sh
+++ rmilter/start.sh
@@ -1,5 +1,19 @@
 #!/bin/sh
 
 rm -f /var/run/rsyslogd.pid
+
+export WHITELIST=$(echo "$RELAYNETS" | sed 's/ /,/g')
+
+# Substitute configuration
+for VARIABLE in `env | cut -f1 -d=`; do
+  sed -i "s={{ $VARIABLE }}=${!VARIABLE}=g" /etc/rmilter.conf
+done
+
+
+if [ "$ANTIVIRUS" == "clamav" ];
+then
+  echo ".try_include /etc/rmilter-clamav.conf" >>  /etc/rmilter.conf
+fi
+
 rmilter -c /etc/rmilter.conf
 rsyslogd -n
